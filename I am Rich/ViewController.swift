cc184b486dadbb81ad62003ae74141425aebfae4
Payton <payton016@gmail.com>
2024-06-12 21:38:18 -0400

    inital
    Signed-off-by: Payton <payton016@gmail.com>

---
+++ I am Rich/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  I am Rich
+//
+//  Created by Payton Mitchell on 6/12/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
+
